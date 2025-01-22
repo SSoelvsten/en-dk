96f8517e3946c7fe2def8b0a91d5b390fe63f262
Steffan Sølvsten <201505832@post.au.dk>
2025-01-22 11:00:23 +0100

    Add tool tips on hover
    This especially will be useful on mobile phones

--- src/RoundButton.tsx
+++ src/RoundButton.tsx
@@ -9,15 +9,18 @@ interface RoundButtonProps {
 };
 
 const RoundButton = ({ onClick, emoji, emojiRotate, title }: RoundButtonProps) =>
-  <button className="RoundButton" onClick={onClick} title={title}>
-    <span className="Icon"
-          style={{
-            transform: `rotate(${emojiRotate ? emojiRotate : 0}deg)`,
-            paddingBottom: emojiRotate && emojiRotate % 90 === 0 ? `0.25rem` : 0
-          }}
-    >
-      {emoji}
-    </span>
-  </button>;
+  <div className="RoundButton">
+    <span className="Title">{title}</span>
+    <button className="Button" onClick={onClick}>
+        <span className="Icon"
+            style={{
+                transform: `rotate(${emojiRotate ? emojiRotate : 0}deg)`,
+                paddingBottom: emojiRotate && emojiRotate % 90 === 0 ? `0.25rem` : 0
+            }}
+        >
+        {emoji}
+        </span>
+    </button>
+  </div>;
 
 export default RoundButton;
